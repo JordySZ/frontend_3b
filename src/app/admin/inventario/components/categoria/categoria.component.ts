88242e21854916b9defe61576bc25b9f7aa78b99
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-12-09 11:00:41 -0500

    lunes

--- src/app/admin/inventario/components/categoria/categoria.component.ts
+++ src/app/admin/inventario/components/categoria/categoria.component.ts
@@ -4,9 +4,9 @@ import { TableModule } from 'primeng/table';
 import { FormGroup,FormControl } from '@angular/forms';
  
 interface Categoria{
-  id?:number,
+  id:number,
   nombre: string;
-  detalle?: string
+  detalle: string
 }
 @Component({
   selector: 'app-categoria',
@@ -17,6 +17,7 @@ export class CategoriaComponent implements OnInit{
   private categoriaService=inject(CategoriaService)
   categorias:Categoria[]=[]
   visible:boolean=false;
+  categoria_id:number=-1;
   categoriaForm=new FormGroup({
     nombre: new FormControl(''),
     detalle: new FormControl('')
@@ -40,15 +41,39 @@ export class CategoriaComponent implements OnInit{
 this.visible=true
   }
   guardarCategoria(){
-this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
+    if(this.categoria_id>0){ 
+this.categoriaService.funModificar(this.categoria_id,this.categoriaForm.value).subscribe(
 (res:any)=>{
   this.visible=false;
   this.getCategorias();
+  this.categoria_id=-1
 },
 (error:any)=>{
   console.log(error);
 }
 )
+}
+else{
+  this.categoriaService.funGuardar(this.categoriaForm.value).subscribe(
+    (res:any)=>{
+      this.visible=false;
+      this.getCategorias();
+      
+    },
+    (error:any)=>{
+      console.log(error);
+    })
+  
+}
+
+  }
+  editarCategoria(cat:Categoria){
+this.visible=true
+this.categoria_id=cat.id
+this.categoriaForm.setValue({nombre: cat.nombre, detalle:cat.detalle})
+  }
+  deleteCategoria(cat:Categoria){
+
   }
 
 }
--- src/app/admin/inventario/services/categoria.service.ts
+++ src/app/admin/inventario/services/categoria.service.ts
@@ -16,5 +16,16 @@ funListar(){
 funGuardar(registro:any)
 {
   return this.http.post(`${this.baseUrl}/categoria`,registro)
-}  //constructor() { }
+  
+}  
+
+funModificar(id:number,registro:any)
+{
+  return this.http.patch(`${this.baseUrl}/categoria/${id}`,registro)
+}  
+
+funEliminar(id:number)
+{
+  return this.http.delete(`${this.baseUrl}/categoria/${id}`)
+}  
 }
