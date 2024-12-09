baca8f9baf47770de15734da3e539140d1acc886
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-12-09 09:27:51 -0500

    lol

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './services/auth.service';
+import { ButtonModule } from 'primeng/button';
+
 
 
 @NgModule({
@@ -21,7 +23,9 @@ import { AuthService } from './services/auth.service';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ButtonModule
+    
   ]
 })
 export class AuthModule { }
